37a42352aab99752aaf0a7147b48451439d1e463
Kasak Gupta <141016805+kasakgupta@users.noreply.github.com>
2024-06-21 09:03:30 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 51522
+let accountEmail = "kasak@gmail.com"
+var accountPassword = "1234"
+accountCity = "Delhi"
+
+// accountId = 02 //not allowed
+
+accountEmail = "kg@gmail.com"
+accountPassword = "5452"
+accountCity = "Mumbai"
+
+console.log(accountId)
+console.table([accountEmail, accountId, accountPassword, accountCity]);
