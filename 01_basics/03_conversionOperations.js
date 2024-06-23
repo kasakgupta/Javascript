3815ef70c5e7573cf9cce7513ed5d856fa7848f0
Kasak Gupta <141016805+kasakgupta@users.noreply.github.com>
2024-06-23 14:58:49 +0000

    3rd commit

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -4,8 +4,8 @@ let score = "33"
 // console.log(typeof (score))
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //"33" => 33
 // "33abc" => NaN
@@ -14,7 +14,47 @@ console.log(valueInNumber);
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false; "kasak" => true
+
+//******** Operations *********
+
+let value = 5
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**4);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " kasak"
+
+let str3 = str1 + str2   //concatenate two strings
+console.log(str3)
+
+
+// console.log("1" + 2)
+// console.log(1 + "1")        // This type of code is never appreciated anywhere
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(+true)    //confusing code
+// console.log(+"")
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
