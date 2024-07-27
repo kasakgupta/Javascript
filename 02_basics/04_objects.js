898e4532bffeeb92ae933c6f6b10220e252b8607
Kasak Gupta <141016805+kasakgupta@users.noreply.github.com>
2024-07-27 15:44:36 +0000

    object2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,34 @@
+// const instaUser = new Object()
+const instaUser = {}
+
+
+instaUser.id = "123@abc"
+instaUser.name = "Ram"
+instaUser.isLoggedIn = false
+
+// console.log(instaUser)
+
+const regularUser = {
+    email: "abc@gmail.com",
+    fullname: {
+        userfullname: {
+            fisrtname: "kasak",
+            lastname: "gupta"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+// const obj3 = { obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2)
+
+const obj3 = {...obj1, ...obj2} //spread operator
+console.log(obj3);
+
+console.log(Object.keys(instaUser));
+console.log(Object.entries(instaUser))
+console.log(instaUser.hasOwnProperty('isLoggedIn'))
