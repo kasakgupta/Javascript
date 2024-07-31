3fb6246caa6c58c652eae32c3f3d6acc3a06c5b7
Kasak Gupta <141016805+kasakgupta@users.noreply.github.com>
2024-07-31 17:03:38 +0000

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,31 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+// Named IIFE
+(function chai(){
+    // This function will be executed immediately after its definition
+    console.log(`DB CONNECTED`);
+})();    //semicolon is mandatory here to execute two iife
+
+// Anonymous IIFE with parameter
+( (name) => {
+    // This function takes a parameter 'name' and logs a message with it
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('kasak'); // The IIFE is immediately invoked with 'kasak' as the argument
+
+
+// Explanation:
+// Named IIFE:
+
+// The function is named chai.
+// It is defined and immediately invoked.
+// The function logs "DB CONNECTED" to the console.
+
+// Anonymous IIFE with Parameter:
+
+// An anonymous arrow function is defined.
+// This function takes one parameter, name.
+// The function logs "DB CONNECTED TWO" followed by the value of name to the console.
+// The IIFE is immediately invoked with 'kasak' as the argument, so it logs "DB CONNECTED TWO kasak".
+
+// IIFE is a design pattern used in JavaScript to execute functions immediately after they are defined.
+//  It helps in creating a local scope and avoiding polluting the global namespace
