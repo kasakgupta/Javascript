ea2f56c1ed838976433ba0bd4ed42331479130e6
Kasak Gupta <141016805+kasakgupta@users.noreply.github.com>
2024-08-01 17:43:51 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,26 @@
+// While loop example
+let index = 0;
+
+// Uncomment the block to execute the while loop
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2; // Increment index by 2
+// }
+
+// While loop to iterate through an array
+let myArray = ['flash', 'batman', 'superman'];
+
+let arr = 0;
+while (arr < myArray.length) {
+    // Uncomment the next line to log each element of the array
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1; // Increment arr by 1
+}
+
+// Do-while loop example
+let score = 11;
+
+do {
+    console.log(`Score is ${score}`);
+    score++; // Increment score by 1
+} while (score <= 10); // The loop will execute at least once, even if the condition is false
